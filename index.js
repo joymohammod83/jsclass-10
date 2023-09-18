169be9536e0891f3970e3e5303b8857a037ff2d9
joymohammod83 <mdjoysarkar83@gmail.com>
2023-09-18 23:32:26 +0600

    js

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ console.log(myFriend)
  let cash = 500;
  let cash1 = 400;
  let cash2 = 120;
- let total = (cash + cash1 + cash2);
- console.log(total);
+ 
+ console.log(cash + cash1 + cash2);
+
+ let bkasAdd = 5000;
+ let cashOut = 3500;
+ let blance =  bkasAdd-cashOut;
+ console.log(blance);
+
+ let cow = "sajol";
+ console.log(cow);
