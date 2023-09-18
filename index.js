eaabda751c9d6303a2dd6c2377291a2510a745e0
joymohammod83 <mdjoysarkar83@gmail.com>
2023-09-18 23:13:41 +0600

    jsclass

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hellow mr joybhai")
+
+let myFriend = "sojol";
+console.log(myFriend)
+
+ let cash = 500;
+ let cash1 = 400;
+ let cash2 = 120;
+ let total = (cash + cash1 + cash2);
+ console.log(total);
