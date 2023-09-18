8f8b4140678fe80505e1d3cf43b1f57657cfda3c
joymohammod83 <mdjoysarkar83@gmail.com>
2023-09-19 00:38:49 +0600

    third

--- index.js
+++ index.js
@@ -3,16 +3,49 @@ console.log("hellow mr joybhai")
 let myFriend = "sojol";
 console.log(myFriend)
 
+let cow = "sajol";
+ console.log(cow);
+
+
  let cash = 500;
  let cash1 = 400;
  let cash2 = 120;
- 
  console.log(cash + cash1 + cash2);
 
  let bkasAdd = 5000;
  let cashOut = 3500;
  let blance =  bkasAdd-cashOut;
- console.log(blance);
+ console.log("sum =", blance);
 
- let cow = "sajol";
- console.log(cow);
+ 
+ let mango = 12;
+ let person = 6;
+ let devag = mango / person;
+ console.log(devag);
+
+ let bhuyaBhill = 3000;
+ let totalPerson = 5;
+ let perPerson = bhuyaBhill / totalPerson;
+ console.log(perPerson);
+
+ let per_mill_cost = 42;
+ let tottal_mill = 50;
+ let tottal_mill_cost = per_mill_cost * tottal_mill;
+ console.log(tottal_mill_cost);
+
+ let sum1 = 20;
+ let sum2 = 3;
+ let multi = sum1 % sum2;
+ console.log(multi);
+
+let scorebord = 199;
+scorebord++;
+console.log(scorebord);
+
+let myGf = 1;
+myGf--;
+console.log( "broken", myGf);
+
+let sajol_gf_list = 3;
+sajol_gf_list+2;
+console.log(sajol_gf_list);
